6998eab7acc9a85ac50088ca9639368854efe6b0
Steven Chen <chensteven0617@gmail.com>
2024-01-25 15:57:05 -0800

    icon version main menu
    supports d-pad, left joystick, and arrow keys navigation

---
+++ Buttons/ui_buttons_icon_version.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@export var button_group: ButtonGroup = null
+var delay = 0.05
+var button_released = null
+# Called when the node enters the scene tree for the first time.
+func _ready(): 
+	for i in get_children():
+		if i is Button:
+			i.connect("pressed", on_button_pressed)
+				
+
+func on_button_pressed():
+	await get_tree().create_timer(delay * 2).timeout
+	var pressed_button = button_group.get_pressed_button()
+	pressed_button.set_pressed_no_signal(false)
+		
