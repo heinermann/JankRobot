3c94ca91c5b8ab249a27378951969803c8023033
Steven Chen <chensteven0617@gmail.com>
2024-01-26 14:32:23 -0800

    merged from main

--- Menus/MainMenu/main_menu.gd
+++ Menus/MainMenu/main_menu.gd
@@ -6,7 +6,7 @@ func _ready():
 	$MenuContainer/MenuVBoxContainer/StartGame.grab_focus()
 
 func _on_start_game_pressed():
-	get_tree().change_scene_to_file("res://[painting] Painting Room/painting room.tscn")
+	get_tree().change_scene_to_file("res://[painting] Painting Room/painting room2.tscn")
 	
 func _on_quit_pressed():
 	get_tree().quit()
