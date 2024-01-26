343bfa6ebb9e83c74de0457534ece5ce6bfa70da
Steven Chen <chensteven0617@gmail.com>
2024-01-25 17:21:20 -0800

    fixed conflicts with main branch and small bugs
    removed menu.cs
    remap signals for buttons
    fixed background image not fit to viewport

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -3,14 +3,19 @@ extends Control
 var controls = preload("res://Controls/controls.tscn").instantiate()
 
 func _ready():
-	%StartGame.grab_focus()
+	$MenuContainer/MenuVBoxContainer/StartGame.grab_focus()
 
 func _on_start_game_pressed():
-	get_tree().change_scene_to_file("res://Painting Room/painting room.tscn")
+	get_tree().change_scene_to_file("res://[painting] Painting Room/painting room.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
 
 func _on_controls_pressed():
-	%MenuContainer.hide()
+	$MenuContainer.hide()
+	$MenuContainer2.hide()
 	get_tree().root.add_child(controls)
+
+func _on_options_pressed():
+	print("Options button pressed");
+	#toggle options menu
