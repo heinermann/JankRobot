bd693c0d4946d30cad05268974b96667fa0d3e29
Adam Heinermann <aheinerm@gmail.com>
2024-01-27 16:19:01 -0800

    Update room with assets

--- [painting] Painting Room/painting room2.gd
+++ [painting] Painting Room/painting room2.gd
@@ -3,7 +3,12 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	$Scientist/AnimationPlayer.play("idle")
+	$Level/Fans/RoomFan/AnimationPlayer.play("Fan")
+	$Level/Fans/RoomFan2/AnimationPlayer.play("Fan")
+	$Level/Fans/RoomFan3/AnimationPlayer.play("Fan")
+	$Level/Fans/RoomFan4/AnimationPlayer.play("Fan")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
