b7fbc08b3204d2811652f4c749e064ee5e59f264
Adam Heinermann <aheinerm@gmail.com>
2024-01-28 04:54:33 -0800

    Added wide paintbrush

--- [painting] Painting Room/DrawViewport.gd
+++ [painting] Painting Room/DrawViewport.gd
@@ -2,11 +2,9 @@ extends SubViewport
 
 @onready var brush: Node2D = $Brush
 
-@export var brush_size: int = 10
-
 func _ready():
 	set_clear_mode(SubViewport.CLEAR_MODE_ONCE)
 
 
 func paint(position: Vector2, colour: Color, texture: Texture2D):
-	brush.queue_brush(position * get_texture().get_height(), colour, brush_size, texture)
+	brush.queue_brush(position * get_texture().get_height(), colour, texture)
