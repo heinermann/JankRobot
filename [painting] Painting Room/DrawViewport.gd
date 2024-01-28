115cd5dfdbcfb17940500df9e49b609486cebd5c
liraymond04 <li.raymond04@gmail.com>
2024-01-27 19:42:13 -0800

    Fix viewport textures not clearing on game start

--- [painting] Painting Room/DrawViewport.gd
+++ [painting] Painting Room/DrawViewport.gd
@@ -4,5 +4,9 @@ extends SubViewport
 
 @export var brush_size: int = 10
 
+func _ready():
+	set_clear_mode(SubViewport.CLEAR_MODE_ONCE)
+
+
 func paint(position: Vector2, colour: Color, texture: Texture2D):
 	brush.queue_brush(position * get_texture().get_height(), colour, brush_size, texture)
