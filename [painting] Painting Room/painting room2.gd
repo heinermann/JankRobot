16b155d65f3f8be4a17b33179d7fcaefc3c40d19
liraymond04 <li.raymond04@gmail.com>
2024-01-27 20:13:28 -0800

    Add timer and option to reduce mood over time

--- [painting] Painting Room/painting room2.gd
+++ [painting] Painting Room/painting room2.gd
@@ -1,5 +1,16 @@
 extends Node3D
 
+var finished_game: bool = false
+
+@export var decrease_mood_over_time: bool = true
+@export var mood_down_over_time: float
+
+var mood: float = 100.0
+
+@export var max_time_in_seconds: float
+var current_time: float = 0
+
+@onready var slider: HSlider = get_node("MoodModule/mood_placeholder")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,17 +22,24 @@ func _ready():
 	$Level/Fans/RoomFan3/AnimationPlayer.play("Fan")
 	$Level/Fans/RoomFan4/AnimationPlayer.play("Fan")
 
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	current_time += delta
+	if current_time > max_time_in_seconds:
+		finish_game()
+		
+	if !finished_game and decrease_mood_over_time:
+		mood -= mood_down_over_time * delta
+		slider.value = mood
+	
 
+func finish_game():
+	finished_game = true
 	var exists = get_node_or_null("/root/PaintingRoom/ResultsMenu")
-	if Input.is_action_just_pressed("ui_page_down") and !exists:
+	if !exists:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		var results_menu = load("res://UI/Menus/ResultsMenu/results_menu.tscn")
 		var instance = results_menu.instantiate()
 		instance.score = $canvas.get_score()
 		add_child(instance)
-
-	pass
